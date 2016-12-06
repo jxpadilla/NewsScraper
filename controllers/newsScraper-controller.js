fdf57024cccd12b84940f7ce81f57e0679590865
Juan Padilla <jxpadilla@yahoo.com>
2016-12-06 14:45:12 -0600

    initial server file, controller & models files

---
+++ controllers/newsScraper-controller.js
@@ -0,0 +1,107 @@
+var cheerio = require('cheerio');
+var express = require('express');
+var mongoose = require('mongoose');
+var request = require('request');
+var router = express.Router();
+
+var Note = require('../models/memo.js');
+var Article = require('../models/Article.js');
+
+router.get('/', function(req, res) {
+    res.render('index');
+});
+
+router.get('/scrape', function(req, res) {
+    request('http://www.recode.net/', function(error, response, html) {
+        if (error) {
+            console.log('Error with request: ' + error);
+        }
+        console.log('Response status code: ' + response.statusCode);
+        if (response.statusCode === 200) {
+            var $ = cheerio.load(html);
+            var resArray = [];
+            $('.c-entry-box__title').each(function(i, element) {
+                var data = {};
+                data.title = $(this).children('a').text();
+                data.link = $(this).children('a').attr('href');
+
+                resArray.push(data);
+
+                var entry = new Article(data);
+
+                entry.save(function(err, doc) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        return;
+                    }
+                })
+            });
+            var hbsObj = { articles: resArray };
+            res.json(hbsObj);
+        }
+    });
+});
+
+router.get('/articles', function(req, res) {
+    Article.find({}, function(err, doc) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(doc);
+        }
+    });
+});
+
+router.get('/articles/:id', function(req, res) {
+    Article.findOne({ '_id': req.params.id })
+        .populate('note')
+        .exec(function(err, doc) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.json(doc);
+            }
+        });
+});
+
+
+router.post('/articles/:id', function(req, res) {
+    var newNote = new Note(req.body);
+    newNote.save(function(err, doc) {
+        if (err) {
+            console.log(err);
+        } else {
+            Article.findOneAndUpdate({ '_id': req.params.id }, { 'note': doc._id })
+                .exec(function(err, doc) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.send(doc);
+                    }
+                });
+        }
+    });
+});
+
+router.delete('/articles/:id', function(req, res) {
+	console.log('req.params.id: ' + req.params.id);
+	Article.findOne({'_id': req.params.id})
+		.exec(function(err, doc) {
+			if (err) {
+				console.log(err);
+			} else {
+				Note.findByIdAndRemove(doc.note, function(err, note) {
+					if (err) {
+						console.log(err);
+					} else {
+						res.send(note);
+					}
+				})
+			}
+		})
+});
+
+console.log('Controller --> Centralized controller (newsScraper-controller.js');
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,66 @@
+// Dependencies
+var bodyParser = require('body-parser');
+var cheerio = require('cheerio');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var request = require('request');
+
+// set up Express app functionality
+var app = express();
+
+
+// To log user activity
+app.use(logger('dev'));
+
+// Express setup to use static files (css, img, etc.) from the '/public' directory
+app.use(express.static(process.cwd() + '/public'));
+
+// Express setup to use body-parser for easy access to request data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+
+// Handlebars view engine
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+
+// Central controller routes
+var routes = require('./controllers/newsScraper-controller.js');
+app.use('/', routes);
+
+
+// connect to localhost MongoDB
+// mongoose.connect('mongodb://localhost/newsScraper');
+// connect to livehost MongoDB
+mongoose.connect('mongodb://heroku_2p385tts:ublglb8ku4ch93e0ji2hh0utav@ds127878.mlab.com:27878/heroku_2p385tts');
+var db = mongoose.connection;
+
+// Display mongoose errors
+db.on('error', function(err) {
+  console.log('Mongoose error: ', err);
+});
+
+// log a successful connection message
+db.once('open', function() {
+  console.log('Mongoose connection successful!');
+});
+
+
+// Express app setup to listen on port 3000
+app.listen(process.env.PORT || 3000, function() {
+	if (process.env.PORT == undefined) {
+		console.log('The server is listening on port 3000');
+	} else {
+    	console.log('The server islistening on port: ' + process.env.PORT);
+    }
+});
